4d41981c849fea03eba5084bd41e455279354948
Cayofds <cayoferreiradesouza@gmail.com>
2025-09-09 09:15:22 -0300

    Funções Math

---
+++ math_funcoes.php
@@ -0,0 +1,23 @@
+<?php
+
+function Adicao($n1,$n2)
+{
+    return $n1 + $n2;
+}
+
+function Subtra($n1,$n2)
+{
+    return $n1 - $n2;
+}
+
+function Multi($n1,$n2)
+{
+    return $n1 * $n2;
+}
+
+function Divisao($n1,$n2)
+{
+    return $n1 / $n2;
+}
+
+?>
